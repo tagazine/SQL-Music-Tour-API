13dc8ef9ac3cf709078afcea8958d59b1560a3b6
Your Namegit config --global user.name Your <you@example.com>
2022-07-09 11:40:44 -0400

    Added Model

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const { Sequelize } = require("sequelize");
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require("dotenv").config();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: 'UNCJeff5000'
+  })
+  
+try {
+    sequelize.authenticate()
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`)
+}
 
 // ROOT
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to the Tour API'
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Tour API",
+  });
+});
 
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+  console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
